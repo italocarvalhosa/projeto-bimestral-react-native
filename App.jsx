546ae2855a60d9530e1dd644b6f9298a96313f56
Jefferson Matheus <jefferson.oliveira203@etec.sp.gov.br>
2024-05-30 19:42:11 -0300

    useFonts-expo-fonts

--- App.jsx
+++ App.jsx
@@ -1,8 +1,14 @@
 import 'react-native-gesture-handler';
-import React from 'react';
 import { RootNavigation } from './src/Screens';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Light': require('./assets/fonts/InterDisplay-ExtraLight.otf'),
+    'Inter-Regular': require('./assets/fonts/InterDisplay-Regular.otf'),
+    'Inter-Medium': require('./assets/fonts/InterDisplay-Medium.otf')
+  });
+  
   return (
     <RootNavigation></RootNavigation>
   );
