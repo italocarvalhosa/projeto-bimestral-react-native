6d96470438b9f8bcd2ef94732c90321597fa3d90
Jefferson Matheus <jefferson.oliveira203@etec.sp.gov.br>
2024-06-02 21:32:28 -0300

    docs/update-doc-functions

--- src/Screens/components/HeaderButton.jsx
+++ src/Screens/components/HeaderButton.jsx
@@ -2,12 +2,26 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Pressable, Text} from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 20
+    }
+});
+
+// Componente que renderiza um botão do lado direito da tela
+// page -> página para qual página vamos navegar
+// text -> texto da página
 const HeaderButton = ({page, text}) => {
 
     const navigation = useNavigation();
 
+    const navigateToScreen = () => {
+        navigation.navigate(page);
+    };
+    // consulte os comentários do arquivo 'ButtonNavigate.jsx'
+    // sobre o metódo "useNavigation" e a função "navigateToScreen"
     return (
-            <Pressable style={styles.container} onPress={() => navigation.navigate(page)}>
+            <Pressable style={styles.container} onPress={navigateToScreen}>
                 <Text>
                     { text }
                 </Text>
@@ -15,11 +29,5 @@ const HeaderButton = ({page, text}) => {
     );
 
 };
-
-const styles = StyleSheet.create({
-    container: {
-        marginLeft: 20
-    }
-});
 
 export { HeaderButton };
